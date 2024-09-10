e52706c1dd4d173500485658b4a212dbb88f7c32
Jalaj07 <jalajvarshney20@gmail.com>
2024-09-10 12:05:50 +0530

    hellooooo

--- src/Components/PlatformIconList.tsx
+++ src/Components/PlatformIconList.tsx
@@ -1,17 +1,40 @@
-import { Text } from "@chakra-ui/react";
+import {
+  FaWindows,
+  FaXbox,
+  FaPlaystation,
+  FaApple,
+  FaLinux,
+  FaAndroid,
+} from "react-icons/fa";
+import { MdPhoneIphone } from "react-icons/md";
+import { SiNintendo } from "react-icons/si";
+import { BsGlobe } from "react-icons/bs";
+import { HStack, Icon } from "@chakra-ui/react";
 import { Platform } from "../Hooks/useGames";
+import { IconType } from "react-icons";
 
 interface Props {
   platforms: Platform[];
 }
 
 const PlatformIconList = ({ platforms }: Props) => {
+  const iconMap: { [key: string]: IconType } = {
+    pc: FaWindows,
+    playstation: FaPlaystation,
+    xbox: FaXbox,
+    nintendo: SiNintendo,
+    mac: FaApple,
+    linux: FaLinux,
+    android: FaAndroid,
+    ios: MdPhoneIphone,
+    web: BsGlobe,
+  };
   return (
-    <>
-      {platforms.map(( platform ) => (
-        <Text>{platform.name}</Text>
+    <HStack marginY={1}>
+      {platforms.map((platform) => (
+        <Icon key={platform.id} as={iconMap[platform.slug]} color="gray.500" />
       ))}
-    </>
+    </HStack>
   );
 };
 
